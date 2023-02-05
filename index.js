0714dc6e992e5cf03bd767aacfc7ecc9d3c69518
developermdcm <54366580+DeveloperMDCM@users.noreply.github.com>
2023-02-05 13:22:34 -0500

    Generador de placas

---
+++ index.js
@@ -0,0 +1,25 @@
+
+var NumberRandom = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+var container = document.querySelector(".container");
+var d ="";
+var len = 0;
+d3.text("dataset_placas.csv", function (data) {
+  var parsedCSV = d3.csv.parseRows(data);
+d  = [parsedCSV];
+ len = parsedCSV.length
+console.log(parsedCSV)
+console.log(len)
+});
+const StartGame = () => { //funcion QUE INICIA el cargue de imagenes
+  for (var i = 1; i < len; i++) {
+    var div = document.createElement("img");
+    container.appendChild(div);
+    div.style.width = `${700}` + `px`;	//`${i}px`
+    div.style.height = `${200}` + `px`; //`${i}px`
+    div.style.margin = `${20}` + `px`; //`${i}px`
+    div.setAttribute("class", `div`);
+    div.setAttribute("src", `https://matriculasdelmundo.com/gCOL1.php?textCOL1=${d[0][i]}&textCOL10=SINCELEJO`); 
+    
+  }
+}
+console.log("mmos")
